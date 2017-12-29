e0274ec10562cc3312ce7c7dbc227c00fd9e65af
jimmychagnaud <chagnaud.jimmy@gmail.com>
2017-12-29 18:09:57 +0100

    idk

--- pages.php
+++ pages.php
@@ -24,9 +24,19 @@ get_header();?>
 				<div class="col-md-6">
 					<div class="row text-center">
 						<div class="col-md-12">
-							<h2>Contactez nous !</h2>
+							<h2>Contactez moi !</h2>
 							<div class="col-md-8 col-md-offset-2" style="margin-top: 50px;">
 								<?php echo do_shortcode(get_field('shortcodeDeContact')); ?>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="row text-center">
+						<div class="col-md-12">
+							<h2>Où me trouver ?</h2>
+							<div class="col-md-8 col-md-offset-2" style="margin-top: 50px;">
+								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46442.1119440441!2d-0.3786763786332114!3d43.3219625516722!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd564885b45c7ae9%3A0x4066517481394b0!2s64000+Pau!5e0!3m2!1sfr!2sfr!4v1514562287563" style="border:0; width:100%; height:40vh" allowfullscreen></iframe>
 							</div>
 						</div>
 					</div>
